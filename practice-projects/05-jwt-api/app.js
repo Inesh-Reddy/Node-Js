0d376a3da90273597e96e8100e1523cf62781034
Inesh-Reddy <ineshreddy007@gmail.com>
2025-01-23 10:51:57 +0530

    implemented routes and basic setup

--- practice-projects/05-jwt-api/app.js
+++ practice-projects/05-jwt-api/app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
 console.log(`JWT authentication - app`);
+
+app.use(express.json());
+
+app.use("/api/v1/jwt");
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}...`);
+});
